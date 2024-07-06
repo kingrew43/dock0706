31c66aa3e49bbb76fda6b14d2ad4bd5d88eba4ad
kingrew43 <165491895+kingrew43@users.noreply.github.com>
2024-07-06 23:30:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { execSync } = require('child_process');
 const FILE_PATH = process.env.FILE_PATH || './temp'; // 运行文件夹，节点文件存放目录
 const projectPageURL = process.env.URL || '';        // 填写项目域名可开启自动访问保活，非标端口的前缀是http://
 const intervalInseconds = process.env.TIME || 120;   // 自动访问间隔时间（120秒）
-const UUID = process.env.UUID || '0ef24cc7-52b2-4270-97c3-cc1ce31205f4';
+const UUID = process.env.UUID || '89c13786-25aa-4520-b2e7-12cd60fb5202';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.abc.cn';      // 哪吒3个变量不全不运行
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';              // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';                   // 哪吒客户端密钥
@@ -19,8 +19,8 @@ const ARGO_AUTH = process.env.ARGO_AUTH || '';                 // 固定隧道js
 const CFIP = process.env.CFIP || 'skk.moe';                   // 优选域名或优选ip
 const CFPORT = process.env.CFPORT || 443;                    // 节点端口
 const NAME = process.env.NAME || 'Vls';                     // 节点名称
-const ARGO_PORT = process.env.ARGO_PORT || 18000;           // Argo端口，使用固定隧道token需和cf后台设置的端口对应
-const PORT = process.env.SERVER_PORT || process.env.PORT || 25000; // 节点订阅端口，若无法订阅请手动改为分配的端口
+const ARGO_PORT = process.env.ARGO_PORT || 8080;           // Argo端口，使用固定隧道token需和cf后台设置的端口对应
+const PORT = process.env.SERVER_PORT || process.env.PORT || 3000; // 节点订阅端口，若无法订阅请手动改为分配的端口
 
 //创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
